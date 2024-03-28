a464b740e13e76719a6a0fdfdaf73f6eb3f77228
nik.205520 <5203330@gmail.com>
2024-03-28 08:10:39 +0300

    edit weatherforecast

--- AniMate-app/Services/UsersService/Models/WeatherForecast.cs
+++ AniMate-app/Services/UsersService/Models/WeatherForecast.cs
@@ -8,5 +8,12 @@ namespace AniMate_app.Services.UsersService.Models
 {
     internal class WeatherForecast
     {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
     }
 }
