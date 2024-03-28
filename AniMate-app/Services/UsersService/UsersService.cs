f2ffa560a90493c32dd0716919df42ac171cd323
nik.205520 <5203330@gmail.com>
2024-03-28 08:06:20 +0300

    add weather forecast

--- AniMate-app/Services/UsersService/UsersService.cs
+++ AniMate-app/Services/UsersService/UsersService.cs
@@ -42,5 +42,17 @@ namespace AniMate_app.Services.UsersService
                 await SecureStorage.Default.SetAsync("Authentication", serializeResponse);
             }
         }
+
+        public async Task<WeatherForecast[]> GetWeatherForeCastData()
+        {
+            var serializedLoginResponseInStorage = await SecureStorage.Default.GetAsync("Authentication");
+            if (serializedLoginResponseInStorage is null) return null!;
+
+            string token = JsonSerializer.Deserialize<LoginResponse>(serializedLoginResponseInStorage)!.AccessToken!;
+            var httpClient = httpClientFactory.CreateClient("custom-httpclient");
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            var result = await httpClient.GetFromJsonAsync<WeatherForecast[]>("/WeatherForecast");
+            return result!;
+        }
     }
 }
